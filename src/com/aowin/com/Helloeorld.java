e11c6cbf93e8322fd8bf42b78dce7ae195858080
fangchanglong <506010774@qq.com>
2018-11-26 14:58:31 +0800

    Initial commit

---
+++ src/com/aowin/com/Helloeorld.java
@@ -0,0 +1,24 @@
+package com.aowin.com;
+
+import java.util.ArrayList;
+
+public class Helloeorld {
+
+
+    public static void main(String [] args) {
+        System.out.print("来了3");
+        int i=0;
+
+        System.out.println("i = " + i);
+        System.out.println(i);
+        ArrayList a=new ArrayList();
+
+        for (Object o : a) {
+            if (a == null) {
+            }
+        }
+    }
+    public void test(){
+
+    }
+}
